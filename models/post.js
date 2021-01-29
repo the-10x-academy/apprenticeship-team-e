7baee9fa6eaa5355110acd655fc09697297f4411
mitesh saini <mitesh0000m@gmail.com>
2021-01-29 11:12:54 +0530

    add models directory and list of collections in it

---
+++ models/post.js
@@ -0,0 +1,31 @@
+var mongoose =require('mongoose')
+var Schema=mongoose.Schema
+var schema =new Schema({
+    filePath:{
+        type:String,
+        required:true
+    },
+    author:{
+        type:String,
+        required:true
+    },
+    location:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:Date,
+        default:Date.now
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    likes:{
+        type:Number,
+        required:true,min:0
+    }
+
+});
+const post=new mongoose.model('post',schema)
+module.exports=post;
