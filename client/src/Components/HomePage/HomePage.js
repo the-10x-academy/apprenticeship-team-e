c1139c30bec945eb89e80d289308ddfc4b0957d4
Pavan Kumar <kandivalasapavankumar@gmail.com>
2021-02-02 09:31:57 +0530

    Added button and image on landing page

--- client/src/Components/HomePage/HomePage.js
+++ client/src/Components/HomePage/HomePage.js
@@ -3,20 +3,17 @@ import React from "react";
 import "./HomePage.css";
 
 class HomePage extends React.Component {
-  render() {
-    return (
-      <>
-        <div className="home_img">
-          <img src="./img/lens-1418954.png" alt="Home" />
-
-          <p className="intro">10x Team 04</p>
-          <button type="button" className="enter">
-            Enter
-          </button>
-        </div>
-      </>
-    );
-  }
+	render() {
+		return (
+			<div className="layout">
+				<img src="./img/lens-1418954.png" alt="Home" />
+				<p>10x Team 04</p>
+				<button type="button" className="enter">
+					Enter
+				</button>
+			</div>
+		);
+	}
 }
 
 export default HomePage;
