f6ed4feb76a65be1d7d17e5262cabefb5c463ee7
Pavan Kumar <kandivalasapavankumar@gmail.com>
2021-02-05 15:08:43 +0530

    added responsiveness to Landing Page

--- client/src/Components/HomePage/HomePage.js
+++ client/src/Components/HomePage/HomePage.js
@@ -5,12 +5,16 @@ import "./HomePage.css";
 class HomePage extends React.Component {
 	render() {
 		return (
-			<div className="layout">
+			<div className="Home">
 				<img src="./img/lens-1418954.png" alt="Home" />
-				<p>10x Team 04</p>
-				<button type="button" className="enter">
-					Enter
-				</button>
+				<div className="txtBtn">
+					<text>10x Team 04</text>
+					<div className="btn">
+						<button type="button" className="enter">
+							Enter
+						</button>
+					</div>
+				</div>
 			</div>
 		);
 	}
