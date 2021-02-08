793b012f268f6b297164398eb025b60a3e61193d
Pavan Kumar <77329032+rpapavan97@users.noreply.github.com>
2021-02-08 22:57:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 app.use("/", indexRouter);
 
