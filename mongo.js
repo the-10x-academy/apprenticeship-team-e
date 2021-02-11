98d2d49f95bb7439f9dc4cc8fcf25bf60efd63e9
Pavan Kumar <kandivalasapavankumar@gmail.com>
2021-02-11 22:05:23 +0530

    Final

--- mongo.js
+++ mongo.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
-const url =
-	"mongodb+srv://instaclone:instaclone@project.hhes2.mongodb.net/instaclone?retryWrites=true&w=majority";
+// const url =
+// 	"mongodb+srv://instaclone:instaclone@project.hhes2.mongodb.net/instaclone?retryWrites=true&w=majority";
+
+const url = "mongodb://127.0.0.1:27017/InstaClone";
 
 mongoose.connect(process.env.MONGODB_URI || url, {
 	useNewUrlParser: true,
